1bdc3649587dab8ab927dcf494fb05463fb2945b
ax6030 <138182326+ax6030@users.noreply.github.com>
2023-06-30 15:29:57 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Sideways
+{
+    using Upside;
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int i = 1;
+            bool b = true;
+            string s = i.ToPrice();
+            s.SendIt();
+            b.Green().SendIt();
+            b = false;
+            b.Green().SendIt();
+            i = 3;
+            i.ToPrice().SendIt();
+        }
+
+        
+    }
+}
